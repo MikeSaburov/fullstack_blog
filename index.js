70daf4153d9e77b998bcf9476a85f59bc74e71d9
Mikhail Saburov <saburov1985@gmail.com>
2023-12-07 20:38:48 +1100

    refactor(index): обурнул post запрос в try/catch

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { json } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
@@ -25,26 +25,30 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', registerValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    //Шифрование пароля
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new userModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
+  } catch (error) {
+    res.json(error);
   }
-
-  //Шифрование пароля
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new userModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json(user);
 });
 
 app.listen(4444, (err) => {
