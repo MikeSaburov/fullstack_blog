a2544a94fed999ca68caf031b5b4f43a4d7f1659
Mikhail Saburov <saburov1985@gmail.com>
2023-12-07 20:41:35 +1100

    refactor(index): добавил вывод ошибки в консоль

--- index.js
+++ index.js
@@ -47,6 +47,7 @@ app.post('/auth/register', registerValidation, async (req, res) => {
 
     res.json(user);
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: 'Не удалось зарегестироваться',
     });
