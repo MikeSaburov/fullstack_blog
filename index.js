6b891d8d95a10dd914996ae338fb7d98696dd76f
Mikhail Saburov <saburov1985@gmail.com>
2023-12-07 22:40:43 +1100

    refactor(UserController): вынес функции в UserController
    вынес все функции в отделную папку controllers и прокинул из в index.js
    что существенно сократило код

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 import express, { json } from 'express';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 
-import { validationResult } from 'express-validator';
 import { registerValidation } from './validations/auth.js';
-
-import userModel from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
 
 //Подключение к базе данных (MongoDB)
 mongoose
@@ -26,121 +23,13 @@ const app = express();
 app.use(express.json());
 
 //Авторизация пользователя
-app.post('/auth/login', async (req, res) => {
-  try {
-    const user = await userModel.findOne({
-      email: req.body.email,
-    });
-
-    //проверка есть ли такой пользователь в БД
-    if (!user) {
-      return res.status(404).json({ message: 'Неверный логин или пароль' });
-    }
-
-    //проверка совпадают ли пароли
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash
-    );
-
-    if (!isValidPass) {
-      return res.status(400).json({ message: 'Неверный логин или пароль' });
-    }
-
-    //создаем токен
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      {
-        expiresIn: '30d',
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: 'Не удалось авторизоваться',
-    });
-  }
-});
+app.post('/auth/login', UserController.login);
 
 //Регистрация пользователя
-app.post('/auth/register', registerValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    //Шифрование пароля
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new userModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    //создаем токен
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      {
-        expiresIn: '30d',
-      }
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-      token,
-    });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({
-      message: 'Не удалось зарегестрироваться',
-    });
-  }
-});
+app.post('/auth/register', registerValidation, UserController.register);
 
 //Получение информации о себе
-app.get('/auth/me', checkAuth, async (req, res) => {
-  try {
-    const user = await userModel.findById(req.userId);
-
-    if (!user) {
-      return res.status(404).json({
-        message: 'Нет такого пользователя',
-      });
-    }
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({
-      ...userData,
-    });
-  } catch (err) {
-    console.log(err);
-    res.status(500).json({
-      message: 'Нет доступа',
-    });
-  }
-});
+app.get('/auth/me', checkAuth, UserController.getMe);
 
 app.listen(4444, (err) => {
   if (err) {
