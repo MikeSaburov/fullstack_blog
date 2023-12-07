c85186b591fdac3988a43f1f7c511f17f7194f69
Mikhail Saburov <saburov1985@gmail.com>
2023-12-07 22:56:02 +1100

    feature(validations): добавил валидацию для создание статьи

--- validations.js
+++ validations.js
@@ -15,3 +15,18 @@ export const registerValidation = [
   body('fullName', 'Укажите имя не менее 3 символов').isLength({ min: 3 }),
   body('avatarUrl', 'Неверная ссылка на аватарку').optional().isURL(),
 ];
+
+export const postCreateValidation = [
+  body('title', 'Введите заголовок статьи')
+    .isLength({
+      min: 3,
+    })
+    .isString(),
+  body('text', 'Введите текст статьи')
+    .isLength({
+      min: 10,
+    })
+    .isString(),
+  body('tags', 'Неверный формат тэгов(укажите массив)').optional().isArray(),
+  body('imageUrl', 'Неверная ссылка на изображение').optional().isString(),
+];
