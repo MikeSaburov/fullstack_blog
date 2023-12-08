6e6cdbaab976e807197cb1e72ffa1fefdf052813
Mikhail Saburov <saburov1985@gmail.com>
2023-12-08 21:42:09 +1100

    feature(PostController): создал контроллер создания статьи

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,21 @@
-export const create = (req, res) => {
+import PostModel from '../models/Post';
+
+export const create = async (req, res) => {
   try {
-  } catch (err) {}
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId,
+    });
+
+    res.json(post);
+    const post = await doc.save();
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: 'Не удалось создать статью',
+    });
+  }
 };
