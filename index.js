37574a44ea7e25c53c661bcef77d506d7eec6a42
Mikhail Saburov <saburov1985@gmail.com>
2023-12-09 15:55:35 +1100

    regfactor(index): Добавил валидацию на обновление статьи

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.post('/posts', checkAuth, postCreateValidation, PostController.create);
 app.delete('/posts/:id', checkAuth, PostController.remove);
 
 /** Изменение статьи */
-app.patch('/posts/:id', checkAuth, PostController.update);
+app.patch('/posts/:id', checkAuth, postCreateValidation, PostController.update);
 
 /**------------------------------------------------------- */
 
