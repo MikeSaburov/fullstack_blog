e1f21289444ce0436a1cc37b206ce03b8c406ea0
Mikhail Saburov <saburov1985@gmail.com>
2023-12-10 22:15:30 +1100

    refactor(PostController, index): поправил плучение тэгов

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ app.post('/upload', checkAuth, upload.single('image'), (req, res) => {
 app.get('/posts', PostController.getAll);
 
 /*Получение тэгов */
+app.get('/tags', PostController.getLastTags);
 app.get('/posts/tags', PostController.getLastTags);
 
 //Получение одной статьи
